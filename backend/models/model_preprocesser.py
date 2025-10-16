8b3265c903018517b8179cef08f60597a930a680
Deep Sahu <deep.sahu1205@gmail.com>
2025-10-16 00:58:09 -0400

    established model baselines

--- backend/models/model_preprocesser.py
+++ backend/models/model_preprocesser.py
@@ -3,8 +3,10 @@ from sklearn.preprocessing import LabelEncoder
 
 
 
-def preprocess_data(df):
+
+def preprocess_data(file_path):
     
+    df = pd.read_csv(file_path)
     # ...CONVERT DATE...
 
     df["date"] = pd.to_datetime(df['date'])
@@ -28,7 +30,42 @@ def preprocess_data(df):
     df["encoded_ticker"] = encoder.fit_transform(df["ticker"])
     df = df.drop(columns = ["ticker"])
     
-    return df
+    # ... Shift and create lag columns... 
+    group = df.groupby("encoded_ticker")
+
+    lag_cols = ["open_price", "high_price", "low_price", "close_price", "volume",
+                "sentiment_score", "polarity", "headline_count"]
+
+    for col in lag_cols:
+        df[f"{col}_lag1"] = group[col].shift(1)
+    
+    df = df.dropna(subset=["open_price_lag1", "close_price_lag1"]) #delete rows with NaN lag values
+
+    # ... Define feature columns ...
+    feature_cols = [
+        # prices known after market opens
+        "open_price",
+        "open_price_lag1", "high_price_lag1", "low_price_lag1",
+        "close_price_lag1", "volume_lag1",
+        # news sentiment — current & lagged
+        "sentiment_score", "polarity", "headline_count",
+        "sentiment_score_lag1", "polarity_lag1", "headline_count_lag1",
+        # calendar & meta
+        "day", "day_of_week", "month", "quarter", "year",
+        "time_index", "encoded_ticker"
+]
+    df_final = df[feature_cols + ["close_price"]].copy()
+
+    return df_final
+
+
+if __name__ == "__main__":
+    input_path = "backend/data/processed_data/merged_data.csv"
+    output_path = "backend/data/processed_data/final_processed_data.csv"
+    
+    processed_df = preprocess_data(input_path)
+    processed_df.to_csv(output_path, index = False)
+
 
 
     
