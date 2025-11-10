9dd1daca382735fa78e4f294d3e59e8c78eadec1
Deep Sahu <deep.sahu1205@gmail.com>
2025-11-10 12:36:52 -0500

    added cat boost and light gbm model baselines

--- backend/models/baseline_models.py
+++ backend/models/baseline_models.py
@@ -3,11 +3,14 @@ from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
 from lightgbm import LGBMRegressor
 from catboost import CatBoostRegressor
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization, Dense, Input, Flatten
+from keras.layers import LSTM, Dense, Dropout
+from keras.layers import MultiHeadAttention, LayerNormalization, Dense, Input, Flatten
 import pandas as pd
-from models.evaluate import evaluate_model
+from evaluate import evaluate_model
+import numpy as np
+
+
+
 
 
 
@@ -48,6 +51,23 @@ rf = RandomForestRegressor(
 )
 
 # Light GBM model
+lgbm = LGBMRegressor(
+    n_estimators = 100,
+    learning_rate = .01,
+    num_leaves = 31,
+    random_state = 42
+)
+
+#Cat boost model
+cat = CatBoostRegressor(
+    iterations=200,
+    learning_rate=0.05,
+    depth=6,
+    loss_function='RMSE',
+    verbose=False,
+    random_seed=42
+)
+
 
 
 
@@ -57,6 +77,10 @@ xg.fit(X_train, y_train)
 print("fitted xgboost model")
 rf.fit(X_train, y_train)
 print("fitted random forest model")
+lgbm.fit(X_train, y_train)
+print("fitted lightGBM model")
+cat.fit(X_train, y_train)
+print("fitted catboost model")
 
 #Record accuracy and evaluate models...
 
@@ -65,7 +89,10 @@ print("fitted random forest model")
 if __name__ == "__main__":
     evaluate_model("XGBoost Regressor", xg, X_train, y_train, X_test, y_test)
     evaluate_model("Random Forest Regressor", rf, X_train, y_train, X_test, y_test)
+    evaluate_model("LightGBM Regressor", lgbm, X_train, y_train, X_test, y_test)
+    evaluate_model("CatBoost Regressor", cat, X_train, y_train, X_test, y_test)
 
 
 
 #implement lightGBM, CatBoost, LSTM, Transformer models later
+
